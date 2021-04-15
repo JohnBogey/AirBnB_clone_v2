e5efdbbf8c6ba04b0efcc1971cf3533ea686c964
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-15 12:28:58 -0400

    removed extra slashes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,8 +2,8 @@
 # set up web server for webstatic deployment
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 sudo printf '%s\n' \
 '<html>' \
 '  <head>' \
@@ -14,8 +14,7 @@ sudo printf '%s\n' \
 '</html>' \
 |sudo tee /data/web_static/releases/test/index.html
 sudo ln -fsn /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-
+sudo chown -R ubuntu:ubuntu /data
 location='location /hbnb_static/ { alias /data/web_static/current/; autoindex off; }'
 sudo sed -i "/listen \[::\]:80 default_server;/a $location" /etc/nginx/sites-enabled/default
 sudo service nginx restart
