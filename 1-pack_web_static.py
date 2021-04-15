30914ad75769eed53f524a18d9682bedb049b4c6
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-15 14:03:36 -0400

    0 putting my code back 1 new code

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' get compressed file of web static '''
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    '''packs webstatic'''
+    now = datetime.now().strftime("%Y%m%d%I%M%S")
+    local("mkdir -p versions")
+    local("tar -cvzf versions/web_static_{}.tgz web_static"
+          .format(now))
