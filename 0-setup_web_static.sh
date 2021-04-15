46127674dc6a46c50c1f5205b0491d78658fa739
JohnBogey <maxwellloganlovell@gmail.com>
2021-04-15 16:36:36 -0400

    fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,8 @@ echo y | sudo ufw enable
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared
 echo '<html>Holberton Scool</html>' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fsn /data/web_static/releases/test/ /data/web_static/current
+sudo rm -f /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 location='location /hbnb_static/ { alias /data/web_static/current/; }'
 sudo sed -i "/listen \[::\]:80 default_server;/a $location" /etc/nginx/sites-enabled/default
